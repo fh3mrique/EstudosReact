477cc2a49d57c3b05b4cb5a42283e68334687d93
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-07-30 11:09:44 -0300

    Comentarios em componentes React

--- 02 - FUNDAMENTOS/fundamentos/src/components/PrimeiroComponente.jsx
+++ 02 - FUNDAMENTOS/fundamentos/src/components/PrimeiroComponente.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+
+//Componente react
 const PrimeiroComponente  = () => {
   return (
     <div>
         <h1>Meu primeiro componente</h1>
+        {/* Diferente do HTML o JSX não usa a palavra class e sim className */}
         <p className='jsx'>JFX</p>
     </div>
   )
