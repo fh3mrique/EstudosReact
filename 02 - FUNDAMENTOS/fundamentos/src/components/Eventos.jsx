1c97d25fe60a8c68251aaed83c5b91efe73079e8
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-07-30 12:33:41 -0300

    Funções de renderização

--- 02 - FUNDAMENTOS/fundamentos/src/components/Eventos.jsx
+++ 02 - FUNDAMENTOS/fundamentos/src/components/Eventos.jsx
@@ -9,6 +9,15 @@ const Eventos = () => {
         console.log("Ativou o evento!!!")
     }
 
+    const conteudoRenderizado = (x) =>{
+        if (x == true){
+           return <h3>REDERIZANDOE ESSE CONTEUDO SE A FUNÇÃO FOR CHAMADO COM TRUE COMO PARAMETRO</h3>
+        }
+        else{
+            return <h3>REDERIZANDOE ESSE CONTEUDO SE A FUNÇÃO FOR CHAMADO COM FALSE COMO PARAMETRO</h3> 
+        }
+    }
+
 
   return (
     <div>
@@ -19,6 +28,9 @@ const Eventos = () => {
         <div>
             <button onClick={() => console.log("Funçao inline chamada")}>INLINE</button>
         </div>
+
+        {conteudoRenderizado(true)}
+        {conteudoRenderizado(false)}
     </div>
   )
 }
