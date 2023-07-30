ec3a19911641a4d842c00202cec9d11523ae7cbb
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-07-30 10:44:54 -0300

    Importando componente

--- 02 - FUNDAMENTOS/fundamentos/src/App.jsx
+++ 02 - FUNDAMENTOS/fundamentos/src/App.jsx
@@ -1,3 +1,7 @@
+//Componentes
+import PrimeiroComponente from './components/PrimeiroComponente'
+
+//Styles / css
 import './App.css'
 
 function App() {
@@ -5,6 +9,7 @@ function App() {
   return (
     <>
       <h1>Fundamentos React</h1>
+      <PrimeiroComponente/>
     </>
   )
 }
