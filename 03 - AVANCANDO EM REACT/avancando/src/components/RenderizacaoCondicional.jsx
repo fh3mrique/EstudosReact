8622de8c1da299d4f05ca97c210c7a5ab05ff4fb
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-08-05 02:21:15 -0300

    if e else no JSX

--- 03 - AVANCANDO EM REACT/avancando/src/components/RenderizacaoCondicional.jsx
+++ 03 - AVANCANDO EM REACT/avancando/src/components/RenderizacaoCondicional.jsx
@@ -3,12 +3,23 @@ import React, { useState } from 'react'
 const  RenderizacaoCondicional = () => {
 
     const [x] = useState(false)
+    const [nome] = useState("Filipe")
+
   return (
     <div>
         <h2>RenderizacaoCondicional</h2>
         <h3>diferentes estados da variavel x iram resulatr em renderizações de paragrafos diferentes abaixo</h3>
         {x && <p>A VARIAVEL X É VERDADEIRA</p>}
         {!x && <p>A VARIAVEL X É FALSA</p>}
+
+        <h3>if ternário</h3>
+        {nome == "Filipe" ? (
+        <div>
+            <p>O nome é Filipe</p>
+        </div>
+        ) : (<div>
+            <p>Nome não encontrado</p>
+        </div>)}
 
     </div>
   )
