769a66819159445de916b1b97313cb3497b78f84
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-08-10 16:30:17 -0300

    Envio de form

--- 05 - FORMULARIO COM REACT/form/src/components/MeuForm.jsx
+++ 05 - FORMULARIO COM REACT/form/src/components/MeuForm.jsx
@@ -11,20 +11,33 @@ const MeuForm = () => {
         setNome(e.target.value)
     }
 
-    console.log(nome)
-    console.log(email)
+    /* console.log(nome)
+    console.log(email) */
+
+    function handleSubmit (evento){
+        /*event.preventDefault() é um método do JavaScript usado na web para evitar que o 
+        navegador siga o comportamento padrão associado a um evento em um elemento HTML. 
+        Isso dá aos desenvolvedores o controle sobre o que acontece quando ocorre um evento,
+        permitindo personalizar as ações em vez de seguir as ações padrão do navegador. */
+        evento.preventDefault()
+        console.log("Enviando formulário")
+        console.log(nome, email)
+    }
+
   return (
     <div>
-        {/* Criação de formúlario */}
-        <form>
+        {/* 4 - Envio de form */}
+        {/* 1- Criação de formúlario */}
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="nome">Nome: </label>
                 <input type="text" name='nome' placeholder='Digite seu nome' onChange={handleName} />
             </div>
 
-            {/* Label envolvendo e-mail */}
+            {/* 2 - Label envolvendo input */}
             <label>
                 <span>E-mail</span>
+                {/* 3 - Simplificando com evento com função inline */}
                 <input type="text" name='email' placeholder='Digite seu email' onChange={(e) => setEmail(e.target.value)}/>
             </label>
 
