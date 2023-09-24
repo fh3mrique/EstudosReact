71e291c35e8ebd57a97726224c5c6fa966299647
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-09-24 16:28:53 -0300

    Escolhendo palavra e categoria de forma aleatória

--- 06 - PROJETO/secretword/src/App.jsx
+++ 06 - PROJETO/secretword/src/App.jsx
@@ -20,8 +20,29 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].nome);
   const [palavras] = useState(wordsList);
 
+  const [palavraSelecionada, setPalavraSelecionada] = useState("");
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState("");
+  
+  const [letras, setLetras] = useState([]);
+
   //Função que inicia o game
   const iniciarGame = () =>{
+   //pegando uma palavra e categoria 
+   const {palavra, categoria} = SelecionarPalavraECategoria();
+
+   //separando a palavra string em array com letras
+   let palavraLetras = palavra.split("");
+   palavraLetras = palavraLetras.map((letra) => letra.toLowerCase());
+   
+
+   console.log(palavra, categoria);
+   console.log(palavraLetras);
+
+   //Preenchendo states
+   setPalavraSelecionada(palavra);
+   setCategoriaSelecionada(categoria);
+   setLetras(letras)
+
     setGameStage(stages[1].nome)
   }
 
@@ -34,6 +55,28 @@ function App() {
   const restartGame = () =>{
     setGameStage(stages[0].nome)
   }
+
+  //Função que escolher uma categória e palavra aleatoria
+  const SelecionarPalavraECategoria = () =>{
+
+    //Pegando uma categoria aleatória
+    const categorias = Object.keys(palavras);
+    const categoria = 
+    categorias[Math.floor(Math.random() * Object.keys(categorias).length)]
+
+    console.log(categoria);
+
+    //pegando uma palavra aleatória
+    const palavra = 
+    palavras[categoria][Math.floor(Math.random() * palavras[categoria].length)]
+
+    console.log(palavra)
+
+    //Agora podemos desestruturar
+    return {palavra, categoria}
+  }
+
+
 
 
   return (
