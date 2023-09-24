0c84a2be141b819e2eda35baa477a5c1c81093c3
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-09-24 08:57:49 -0300

    Criando components do projeto

--- 06 - PROJETO/secretword/src/App.jsx
+++ 06 - PROJETO/secretword/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
 
+//imports css
 import './App.css'
+//imports react
+import { useState, useEffect, useCallback } from 'react'
+//imports dados
+import{wordsList} from "./dados/Palavras"
+//imports components
 import TelaInicial from './components/TelaInicial'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+const stages = [
+  {id:1, nome: 'inicio'},
+  {id:2, nome: 'game'},
+  {id:3, nome: 'fim'},
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].nome);
+  const [palavras] = useState(wordsList);
+
+  console.log(palavras)
 
   return (
     <div className='App'>
-      <TelaInicial/>
-        
+      {gameStage === 'inicio' && <TelaInicial/>}
+      {gameStage === 'game' && <Game/>}
+      {gameStage === 'fim' && <GameOver/>}        
     </div>
   )
 }
