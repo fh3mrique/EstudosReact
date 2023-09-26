5725a2f76bdef254a93db7904aabba05cd8e9407
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-09-26 18:30:45 -0300

    Resgatando Dados com React

--- 07 - REQUISIÇÕES HTTP COM REACT/http/src/App.jsx
+++ 07 - REQUISIÇÕES HTTP COM REACT/http/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
-import './App.css'
+import "./App.css";
+
+//URL onde está rodando os recursos da api
+const url = "http://localhost:3000/products";
 
 function App() {
+  //Local onde os dados do recursos serão armazenados
+  const [produtos, setProdutos] = useState([]);
 
+  //1 - Resgatando dados do recurso
+  useEffect(() => {
+    async function fetchDados() {
+      const resposta = await fetch(url); //dados vem em json
 
+      // Aguarde a resolução da Promise do json()
+      const dados = await resposta.json();
+
+      setProdutos(dados);
+    }
+
+    fetchDados()
+  }, []);
+
+  console.log(produtos);
 
   return (
     <>
       <h1>Api lista de produtos</h1>
+      <ul>
+        {produtos.map((produto) => (
+          <li key={produto.id}>
+            {produto.name} - {produto.price}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
